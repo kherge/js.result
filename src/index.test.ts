f1beb5a6bef604a9feae8557bfcb980c29c9ce18
Kevin Herrera <kevin@herrera.io>
2021-03-29 16:42:16 -0700

    Improving testing.
    Actually testing the objects, consequently removing code smell in Sonar.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,41 @@
 import { Option, Result, err, none, ok, some } from '../src';
 
+type TestError = {
+  error: string;
+};
+
+type TestValue = {
+  value: string;
+};
+
 describe('The index', () => {
   test('should re-export Option', () => {
-    let option: Option<boolean>;
+    let option: Option<TestValue>;
+    const value: TestValue = { value: 'value' };
 
     option = none();
-    option = some(true);
 
-    expect(option).not.toBeUndefined();
+    expect(option.isNone()).toBe(true);
+
+    option = some(value);
+
+    expect(option.isSome()).toBe(true);
+    expect(option.unwrap()).toBe(value);
   });
 
   test('should re-export Result', () => {
-    let result: Result<string, number>;
+    const error: TestError = { error: 'error' };
+    let result: Result<TestValue, TestError>;
+    const value: TestValue = { value: 'value' };
 
-    result = err(123);
-    result = ok('test');
+    result = err(error);
 
-    expect(result).not.toBeUndefined();
+    expect(result.isErr()).toBe(true);
+    expect(result.unwrapErr()).toBe(error);
+
+    result = ok(value);
+
+    expect(result.isOk()).toBe(true);
+    expect(result.unwrap()).toBe(value);
   });
 });
